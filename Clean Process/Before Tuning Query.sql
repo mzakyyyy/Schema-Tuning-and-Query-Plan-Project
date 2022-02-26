7829947b11b3efc96840b916d9447df1bccf2070
mzakyyyy <mzakyy0@gmail.com>
2022-02-26 16:44:35 +0700

    a little bit changes

--- Clean Process/Before Tuning Query.sql
+++ Clean Process/Before Tuning Query.sql
@@ -3,3 +3,11 @@ EXPLAIN ANALYZE
 SELECT p.name
 FROM physician p, affiliated_with aw, trained_in ti, department d  
 WHERE p.employeeid = aw.physician AND aw.department = d.departmentid AND p.employeeid = ti.physician AND d.name = 'Surgery';
+
+-- Query Kasus 2
+SELECT m.name, SUM(CAST(p.dose AS INTEGER)), DATE_PART(‘MONTH’, p.date) AS BULAN, DATE_PART(‘YEAR’, p.date) AS TAHUN
+FROM prescribes p 
+JOIN medication m 
+ON p.medication = m.code
+GROUP BY m.code, DATE_PART(‘MONTH’, p.date), DATE_PART(‘YEAR’, p.date)
+ORDER BY DATE_PART(‘YEAR’, p.date), date_part(‘MONTH’, p.date);
